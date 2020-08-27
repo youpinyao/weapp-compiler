617209ec6c35dfedd90e4ad0108a6c3ebccd6ba8
youpinyao <497400448@qq.com>
2020-08-27 15:12:58 +0800

    refactor: 环境变量替换方式修改

--- build/gulp/loader/env.js
+++ build/gulp/loader/env.js
@@ -4,8 +4,7 @@ module.exports = () =>
   through.obj(function (file, enc, cb) {
     let contents = file.contents.toString();
 
-    contents = contents.replace(/process\.env\.NODE_ENV /g, `'${process.env.NODE_ENV}' `);
-    contents = contents.replace(/process\.env\.NODE_ENV\)/g, `'${process.env.NODE_ENV}')`);
+    contents = contents.replace(/process\.env\.NODE_ENV/g, `'${process.env.NODE_ENV}'`);
 
     file.contents = Buffer.from(contents);
 
