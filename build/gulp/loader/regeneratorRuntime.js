ae84c54093646dba45e56b0929c25343d3a1e17e
youpinyao <497400448@qq.com>
2020-10-19 13:30:03 +0800

    fix: path.retive win32 路径斜杆问题

--- build/gulp/loader/regeneratorRuntime.js
+++ build/gulp/loader/regeneratorRuntime.js
@@ -1,11 +1,18 @@
 const through = require('through2');
+const os = require('os');
+const platform = os.platform();
 const path = require('path');
 
 module.exports = (config, to) =>
   through.obj(function (file, enc, cb) {
+    let runPath = path.relative(path.resolve(to, '../'), path.resolve(config.output, 'regeneratorRuntime.js'));
+
+    if (platform === 'win32') {
+      runPath = runPath.replace(/\\/g, '/');
+    }
     file.contents = Buffer.from(`
       "use strict";
-      var regeneratorRuntime = require("${path.relative(path.resolve(to, '../'), path.resolve(config.output, 'regeneratorRuntime.js'))}");
+      var regeneratorRuntime = require("${runPath}");
       ${file.contents.toString().replace('"use strict";', '')}
     `);
     cb(null, file);
