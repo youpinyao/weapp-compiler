f0e34c9fb03a225c4437288b33a3182d996b8021
youpinyao <497400448@qq.com>
2021-02-24 14:30:03 +0800

    feat: 注释缓存功能

--- build/compiler/copy.js
+++ build/compiler/copy.js
@@ -1,41 +1,41 @@
 const fse = require('fs-extra');
-const hasha = require('hasha');
-const path = require('path');
+// const hasha = require('hasha');
+// const path = require('path');
 
 const gulpSass = require('../gulp/sass');
 const gulpLess = require('../gulp/less/index');
 const gulpJs = require('../gulp/js');
 const gulpFile = require('../gulp/file');
 const config = require('../utils/config')();
-const hash = require('../utils/hash');
+// const hash = require('../utils/hash');
 
 module.exports = async (from, to) => {
   if (!from || !to) {
     throw new Error(`文件路径不能为空 from:${from} to:${to}`);
   }
-  const fromFileHash = await hasha.fromFile(from, { algorithm: 'md5' });
-  const toPath = to.replace(/\.(scss|less)$/g, '.wxss');
-  const tempPath = path.resolve(hash.hashFileDir, encodeURIComponent(toPath));
-  let toFileHash = '';
-  let tempFileHash = '';
+  // const fromFileHash = await hasha.fromFile(from, { algorithm: 'md5' });
+  // const toPath = to.replace(/\.(scss|less)$/g, '.wxss');
+  // const tempPath = path.resolve(hash.hashFileDir, encodeURIComponent(toPath));
+  // let toFileHash = '';
+  // let tempFileHash = '';
 
-  if (fse.existsSync(toPath)) {
-    toFileHash = await hasha.fromFile(toPath, { algorithm: 'md5' });
-  }
-  if (fse.existsSync(tempPath)) {
-    tempFileHash = await hasha.fromFile(tempPath, { algorithm: 'md5' });
-  }
+  // if (fse.existsSync(toPath)) {
+  //   toFileHash = await hasha.fromFile(toPath, { algorithm: 'md5' });
+  // }
+  // if (fse.existsSync(tempPath)) {
+  //   tempFileHash = await hasha.fromFile(tempPath, { algorithm: 'md5' });
+  // }
 
-  if (hash.get(from) === fromFileHash) {
-    // 完全匹配就直接返回
-    if (toFileHash === hash.get(toPath)) {
-      return false;
-    }
-    if (tempFileHash) {
-      fse.copySync(tempPath, toPath);
-      return false;
-    }
-  }
+  // if (hash.get(from) === fromFileHash) {
+  //   // 完全匹配就直接返回
+  //   if (toFileHash === hash.get(toPath)) {
+  //     return false;
+  //   }
+  //   if (tempFileHash) {
+  //     fse.copySync(tempPath, toPath);
+  //     return false;
+  //   }
+  // }
 
   if (/(\.scss)$/g.test(from)) {
     await gulpSass(from, to);
@@ -49,10 +49,10 @@ module.exports = async (from, to) => {
     fse.copySync(from, to);
   }
 
-  toFileHash = await hasha.fromFile(toPath, { algorithm: 'md5' });
+  // toFileHash = await hasha.fromFile(toPath, { algorithm: 'md5' });
 
-  hash.set(from, fromFileHash);
-  hash.set(toPath, toFileHash);
+  // hash.set(from, fromFileHash);
+  // hash.set(toPath, toFileHash);
 
-  fse.copySync(toPath, tempPath);
+  // fse.copySync(toPath, tempPath);
 };
