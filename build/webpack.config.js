53a76bba1f5c7b3096074ab06be7c7231b52797f
youpinyao <497400448@qq.com>
2021-12-21 09:27:38 +0800

    feat: less loader javascriptEnabled

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -288,6 +288,7 @@ module.exports = (options, { analyzer, quiet } = {}) => {
                     options: {
                       lessOptions() {
                         return {
+                          javascriptEnabled: true,
                           paths: [context],
                         };
                       },
