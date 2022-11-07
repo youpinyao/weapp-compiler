3bd0c5830be2afc53e838358a3b94c0e9e60144d
youpinyao <497400448@qq.com>
2022-11-07 15:18:02 +0800

    fix: all 情况下 app.json 同步

--- build/utils/chooseSubpackages.js
+++ build/utils/chooseSubpackages.js
@@ -21,34 +21,35 @@ async function chooseSubpackages() {
   });
 
   if (type === 'custom') {
-    const custom = await inquirer.prompt({
-      type: 'checkbox',
-      name: 'subpackages',
-      message: '请选择需要构建的包',
-      loop: false,
-      pageSize: subpackages.length + 1,
-      choices: [
-        {
-          name: '主包',
-          value: 'app',
-          checked: true,
-          disabled: true,
-        },
-        ...subpackages.map((item) => {
-          return {
-            name: item.root,
-            value: item.root,
-          };
+    {
+      const custom = await inquirer.prompt({
+        type: 'checkbox',
+        name: 'subpackages',
+        message: '请选择需要构建的包',
+        loop: false,
+        pageSize: subpackages.length + 1,
+        choices: [
+          {
+            name: '主包',
+            value: 'app',
+            checked: true,
+            disabled: true,
+          },
+          ...subpackages.map((item) => {
+            return {
+              name: item.root,
+              value: item.root,
+            };
+          }),
+        ],
+      });
+      setAppConfig({
+        ...appConfig,
+        subpackages: subpackages.filter((item) => {
+          return custom.subpackages.includes(item.root);
         }),
-      ],
-    });
-
-    setAppConfig({
-      ...appConfig,
-      subpackages: subpackages.filter((item) => {
-        return custom.subpackages.includes(item.root);
-      }),
-    });
+      });
+    }
   }
 }
 
