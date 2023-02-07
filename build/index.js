60e78e01ed5541ef2e8f3a9d281919129c3a4f4b
youpinyao <497400448@qq.com>
2023-02-07 14:27:33 +0800

    fix: add build env support

--- build/index.js
+++ build/index.js
@@ -3,7 +3,9 @@
 const { program } = require('commander');
 const path = require('path');
 const fs = require('fs');
-const { version } = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')).toString());
+const { version } = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../package.json')).toString(),
+);
 const compiler = {
   dev: require('./compiler/development'),
   build: require('./compiler/production'),
@@ -14,9 +16,19 @@ program.command('dev').action((...args) => {
   process.env.NODE_ENV = 'development';
   compiler.dev();
 });
-program.command('build').action((...args) => {
-  process.env.NODE_ENV = 'production';
-  compiler.build();
-});
+program
+  .command('build')
+  .option('-d, --development', 'build in NODE_ENV = development')
+  .option('-s, --simulation', 'build in NODE_ENV = simulation')
+  .action((options) => {
+    if (options.development) {
+      process.env.NODE_ENV = 'development';
+    } else if (options.simulation) {
+      process.env.NODE_ENV = 'simulation';
+    } else {
+      process.env.NODE_ENV = 'production';
+    }
+    compiler.build();
+  });
 
 program.parse(process.argv);
