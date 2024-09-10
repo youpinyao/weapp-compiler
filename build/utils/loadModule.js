57203297292f58dbce2f2093a881fd7e2443b5f0
youpinyao <youpinyao@anta.com>
2024-09-10 17:53:58 +0800

    fix: loadModule超时报错

--- build/utils/loadModule.js
+++ build/utils/loadModule.js
@@ -1,11 +1,7 @@
 function loadModule(file) {
-  const timer = setTimeout(() => {
-    throw new Error(`${file} loadModule 超时`);
-  }, 2000);
   return new Promise((resolve) => {
     this.addDependency(file);
     this.loadModule(file, (err, src) => {
-      clearTimeout(timer);
       if (err) {
         // console.log();
         // console.log(file, this.resourcePath);
